1d02ec36ab070af643fb7a8544bd9ca9bc602ea4
BismaHaroon <bisma.haroon024@gmail.com>
2023-10-05 22:19:39 +0500

    updated table

--- Dockerfile
+++ Dockerfile
@@ -2,3 +2,5 @@ FROM mysql:latest
 
 ENV MYSQL_ROOT_PASSWORD bmsmlops123
 ENV MYSQL_DATABASE database
+
+
--- Users.sql
+++ Users.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE user_database;
 USE user_database;
 
+
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
